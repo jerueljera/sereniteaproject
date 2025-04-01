1df5f14846b70bcd6e75fc300756ed6fd914b2ba
jerueljera <jeruel.ajera18@gmail.com>
2025-04-01 12:19:11 -0400

    Delete web directory

--- web/delete_item.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'connect.php';
-$id = $_POST['id'];
-$conn->query("DELETE FROM stock WHERE id = $id");
-?>
--- web/fetch_items.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'connect.php';
-$result = $conn->query("SELECT * FROM stock ");
-$items = [];
-while ($row = $result->fetch_assoc()) {
-    $items[] = $row;
-}
-echo json_encode($items);
-?>
